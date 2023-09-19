76d20263eca6a528a6e721e24d21c8a302000077
Rainscall <admin@typeboom.com>
2023-09-19 13:20:19 +0800

    modified:   index.html 	modified:   main.js 	new file:   pwdDir.txt

--- main.js
+++ main.js
@@ -70,9 +70,57 @@ function closeQRCode() {
     document.getElementById('qrcodeError').style.display = 'none';
 }
 
-function clearElement() {
-    var element = document.getElementById("qrcode");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+async function decryptFile() {
+    const originInput = document.getElementById('originInput');
+    const ciphertext = originInput.value;
+    const successfulResults = []; // 用于记录所有成功的解密结果
+
+    try {
+        const response = await fetch('/pwdDir.txt');
+        if (!response.ok) {
+            throw new Error('Failed to fetch file');
+        }
+
+        const fileContent = await response.text();
+        const keyLines = fileContent.split('\n');
+
+        for (const key of keyLines) {
+            try {
+                const decryptedValue = decryptAES256(ciphertext, key.trim());
+                if (decryptedValue) {
+                    successfulResults.push({
+                        key: key.trim(),
+                        value: decryptedValue
+                    });
+                }
+            } catch (error) {
+                // 处理解密错误，继续尝试下一个密钥
+                console.error('Decryption error: ' + error.message);
+            }
+        }
+
+        if (successfulResults.length > 0) {
+            // 输出成功解密的结果
+            originInput.value = '#Results are for reference only and not guaranteed for correctness.\n#The following will output all possible keys found and the original text\n#plaintext                        key \n\n' +
+                successfulResults.map(result =>
+                    `${result.value}                 ${result.key}`
+                ).join('\n');
+            return successfulResults.map(result => result.value);
+        } else {
+            originInput.value = 'Decryption failed';
+            return 'failed';
+        }
+    } catch (error) {
+        console.error('An error occurred: ' + error.message);
+        originInput.value = 'An error occurred: ' + error.message;
+        return 'failed';
     }
 }
+
+
+
+
+
+
+
+
